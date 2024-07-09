ddc38185a62377634775b98f352c3e84a06063f1
nayere <rebecca.nayere@gmail.com>
2024-07-09 12:23:48 +0300

    modify homescreen file

--- src/screens/HomeSreen.tsx
+++ src/screens/HomeSreen.tsx
@@ -1,21 +1,52 @@
+// src/screens/HomeScreen.tsx
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../types';
-
-type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
-
-type Props = {
-  navigation: HomeScreenNavigationProp;
-};
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Props } from '../types';
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
-      <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 export default HomeScreen;
+
+
+
+
+// import React from 'react';
+// import { View, Text, Button } from 'react-native';
+// import { StackNavigationProp } from '@react-navigation/stack';
+// import { RootStackParamList } from '../types';
+
+
+// type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
+// type Props = {
+//   navigation: HomeScreenNavigationProp;
+// };
+
+// const HomeScreen: React.FC<Props> = ({ navigation }) => {
+//   return (
+//     <View>
+//       <Text>Home Screen</Text>
+//       <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+//     </View>
+//   );
+// };
+
+// export default HomeScreen;
--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,11 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
     Home: undefined;
-    Details: { itemId: number; otherParam: string };
+    Details: undefined;
+    // Details: { itemId: number; otherParam: string };
     JournalEntry: undefined;
     Login: undefined;
     Signup: undefined;
   };
   
+  export type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
