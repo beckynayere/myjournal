8b71a9240bf5e0892c8f9358d50a1563627c1451
nayere <rebecca.nayere@gmail.com>
2024-07-09 10:17:48 +0300

    modify metro config file

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,25 @@
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = {
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig(__dirname);
+  return {
     transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
       getTransformOptions: async () => ({
         transform: {
           experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
+          inlineRequires: true
+        }
+      })
     },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      extraNodeModules: {
+        src: __dirname + '/src'
+      }
+    }
   };
-  
+})();
